c81a77f266cd85cf828378af94756edfde2d1da6
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2023-11-13 21:01:09 +0530

    Add JsonProblem and related types to pkg/types

---
+++ pkg/types/problem.go
@@ -0,0 +1,5 @@
+package types
+
+type JsonProblem struct {
+	Problem
+}
---
+++ pkg/types/types.go
@@ -0,0 +1,24 @@
+package types
+
+import "time"
+
+type Problem struct {
+	Question string
+	Answer   string
+}
+
+// an error made by the user in answering the question
+type UserError struct {
+	GivenProb Problem
+	UserAns   string
+	QuesNo    int
+}
+
+type ReaderFunc func() (string, error)
+
+type TimerFunc func(t TimeConf) <-chan time.Time
+
+type TimeConf struct {
+	Time int
+	Unit string
+}
