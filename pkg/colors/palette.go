3387379619ce72da89878e6d98e6a3956dbd4cfa
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-01-04 00:41:58 +0530

    add colors for success,warning,error

---
+++ pkg/colors/palette.go
@@ -0,0 +1,13 @@
+package colors
+
+import "github.com/charmbracelet/lipgloss"
+
+var GraveError = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("196"))
+var GreatSuccess = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("40"))
+var MuchWarning = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("184"))
