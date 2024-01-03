b40bd29103ad7f1ba094c8698a8754349e574ae8
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-01-03 12:06:12 +0530

    add huh to dependencies

--- pkg/testUser/MCQ.go
+++ pkg/testUser/MCQ.go
@@ -1,8 +1,24 @@
 package testUser
 
-import "quiz/pkg/types"
+import (
+	"fmt"
+	"quiz/pkg/types"
+
+	"github.com/charmbracelet/huh"
+)
 
 func testMCQ(problem types.Problem) int {
-
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().Title(problem.Question).
+				Options(
+					huh.NewOption("A", "val"),
+				),
+		),
+	)
+	err := form.Run()
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
 	return problem.MarksIfCorrect
 }
