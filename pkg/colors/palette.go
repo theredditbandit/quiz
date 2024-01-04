7e4e2bf1df3646f99e5aa7f4f2e7ad4e4b3a22e0
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-01-04 17:11:23 +0530

    add color for help text

--- pkg/colors/palette.go
+++ pkg/colors/palette.go
@@ -5,12 +5,19 @@ import "github.com/charmbracelet/lipgloss"
 var GraveError = lipgloss.NewStyle().
 	Bold(true).
 	Foreground(lipgloss.Color("196"))
+
 var GreatSuccess = lipgloss.NewStyle().
 	Bold(true).
 	Foreground(lipgloss.Color("40"))
+
 var MuchWarning = lipgloss.NewStyle().
 	Bold(true).
 	Foreground(lipgloss.Color("184"))
+
 var NormalText = lipgloss.NewStyle().
 	Bold(true).
 	Foreground(lipgloss.Color("86"))
+
+var helpStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#626262")).
+	Render
